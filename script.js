fef156e3b6ce4b7f41b79cf7ccc6748711012207
navah21 <164158949+navah21@users.noreply.github.com>
2024-04-29 13:03:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+ var a=getelementById("firstname");
+ var b=getelementById("latname")
+ var c=getelementById("email")
+var d=getelementById("pass")
+function password(){
+    getelementById("pass")
+
+    alert("password incorrect" )
+}
